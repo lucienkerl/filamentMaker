19cade824803155fbb0939ce3ca74859c2738133
Lucien Kerl <lucien@kerl.io>
2021-04-23 17:16:30 +0200

    working

--- filamentMaker/src/main.cpp
+++ filamentMaker/src/main.cpp
@@ -1,65 +1,89 @@
 #include <Arduino.h>
+/*
+  (c)2018 Pawel A. Hernik
+  YouTube video:
+  https://youtu.be/RIt08GJH2IA
+*/
 
-// Pin Declarations
-int dataIn = 11;
-int clockIn = 12;
+/*
+   Cheap digital caliper pinout (from top left):
+   #1 - GND (black)
+   #2 - DAT (green)
+   #3 - CLK (blue)
+   #4 - VCC 1.5-1.8V (red)
+ */
 
-// Variables
-int clockVar = 1;
-int lastClock = 1;
-unsigned long timeVar = 0;
-unsigned long timeVarStart = 0;
-int out = 0;
+#define CLOCK_PIN D1
+#define DATA_PIN D2
+
+//#include <OLEDSoftI2C_SH1106.h>
+// define USEHW in above header for hw I2C version
+
+//OLEDSoftI2C_SH1106 oled(0x3c);
+void readCaliper();
 
 void setup()
 {
-  // Pin Set Up
-  pinMode(dataIn, INPUT);
-  pinMode(clockIn, INPUT);
-
   Serial.begin(115200);
-  Serial.println("Ready: ");
+  Serial.println("Ready");
+  pinMode(CLOCK_PIN, INPUT);
+  pinMode(DATA_PIN, INPUT);
 }
 
-int analogReadToDigital()
-{
-  return analogRead(dataIn) < 820 ? 0 : 1; // If analog read is below 4V = Digital Low
-}
+char buf[20];
+unsigned long tmpTime;
+int sign;
+int inches;
+long value;
+float result;
 
 void loop()
 {
-  return;
-  lastClock = clockVar;
-  if (analogRead(clockIn) < 820) // If analog read is below 4V = Digital Low
+  while (digitalRead(CLOCK_PIN) == HIGH)
   {
-    clockVar = 0;
   }
-  else
+  tmpTime = micros();
+  while (digitalRead(CLOCK_PIN) == LOW)
   {
-    clockVar = 1;
   }
+  if ((micros() - tmpTime) < 500)
+    return;
+  readCaliper();
+  Serial.println(result, 2);
+  Serial.println(inches);
+  buf[0] = ' ';
+  //dtostrf(result, 6, 3, buf + 1);
+  //strcat(buf, " in ");
+  dtostrf(result * 2.54, 6, 3, buf + 1);
+  strcat(buf, " cm ");
+  Serial.println(buf);
 
-  if (lastClock == 1 && clockVar == 0)
-  {
-    out = analogReadToDigital() + analogReadToDigital() + analogReadToDigital(); // Tripple sampling to remove glitches
-    if ((micros() - timeVar) > 800)
-    {
-      Serial.println(" ");
-    }
-    else if ((micros() - timeVar) > 400)
-    {
-      Serial.print("  ");
-    }
+  Serial.println("in loop");
+  delay(500);
+}
 
-    if (out > 1)
+void readCaliper()
+{
+  sign = 1;
+  value = 0;
+  inches = 1;
+  for (int i = 0; i < 24; i++)
+  {
+    while (digitalRead(CLOCK_PIN) == HIGH)
     {
-      Serial.print("1");
     }
-    else
+    while (digitalRead(CLOCK_PIN) == LOW)
     {
-      Serial.print("0");
     }
-    Serial.print(",");
-    timeVar = micros();
+    if (digitalRead(DATA_PIN) == LOW)
+    {
+      if (i < 20)
+        value |= (1 << i);
+      if (i == 20)
+        sign = -1;
+      if (i == 23)
+        inches = 1; // doesn't work with my caliper, always returns inches
+    }
   }
+  result = (value * sign) / (inches ? 2000.0 : 100.0);
 }
